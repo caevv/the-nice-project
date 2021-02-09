982561d4b04c16a907b80fa067007d4923fa91a7
Carlos Vieira <cae.vieira@gmail.com>
2021-02-09 23:48:01 +0000

    initial commit

---
+++ backend/customer/update.ts
@@ -0,0 +1,11 @@
+import {APIGatewayProxyEvent, APIGatewayProxyResult, Context} from "aws-lambda"
+
+export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Hello world',
+            input: event,
+        })
+    }
+}
